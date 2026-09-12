53cc0c709db5f71cde8f23a1184a00697574467a
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-12 13:21:36 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (70.79%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -4,7 +4,7 @@ class Solution {
 
         
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
+            for (int j =0; j < i; j++) {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
