4b7fa51bc4eb9de49aba24cc6c0c38e519a6e756
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-19 15:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (29.14%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0 , hi = nums.length;
+        while(lo < hi){
+            int mid = lo + (hi-lo)/2;
+            if(nums[mid] < target){
+                  
+                lo = mid+1;
+
+            } else{
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+}
