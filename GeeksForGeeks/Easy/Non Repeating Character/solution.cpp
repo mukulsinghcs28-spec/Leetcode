e52008540f3d0fc615863705dd2e9baaebb7b6d6
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-22 13:11:04 +0530

    Solved: Non Repeating Character [Easy]

---
+++ GeeksForGeeks/Easy/Non Repeating Character/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public char nonRepeatingChar(String s) {
+        // code here
+        int[]freq = new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+       int max =2;
+        char ans = 'a';
+        for(int i = 0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']==1){
+                return s.charAt(i);
+            }
+        }
+        return '$';
+    }
+}
