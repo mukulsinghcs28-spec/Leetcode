eada26fb4485988dcefdb5cc8d2d7c5eb952bf5c
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-23 20:22:03 +0530

    Time: 5 ms (44.33%) | Memory: 44.1 MB (26.94%) - LeetSync

---
+++ 851-goat-latin/goat-latin.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String toGoatLatin(String sentence) {
+
+        String[] words = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+
+            String word = words[i];
+
+            
+            if ("aeiouAEIOU".indexOf(word.charAt(0)) == -1) {
+                word = word.substring(1) + word.charAt(0);
+            }
+
+            
+            word += "ma";
+
+            for (int j = 0; j <= i; j++) {
+                word += "a";
+            }
+
+            ans.append(word);
+
+            
+            if (i < words.length - 1) {
+                ans.append(" ");
+            }
+        }
+
+        return ans.toString();
+    }
+}
