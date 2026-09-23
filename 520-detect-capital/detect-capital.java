a89efa12210a3512a4324f376aa7c7503f2911d8
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-23 23:48:26 +0530

    Time: 1 ms (89.17%) | Memory: 43.3 MB (25.84%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capital = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isUpperCase(word.charAt(i))) {
+                capital++;
+            }
+        }
+
+        return capital == 0 ||
+               capital == word.length() ||
+               (capital == 1 && Character.isUpperCase(word.charAt(0)));
+    }
+}
