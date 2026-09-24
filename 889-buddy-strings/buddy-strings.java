137ae1ed6bc083d225224a6f64b1b3e25841a4f6
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-24 06:33:50 +0530

    Time: 2 ms (65.92%) | Memory: 43.8 MB (46.68%) - LeetSync

--- 889-buddy-strings/buddy-strings.java
+++ 889-buddy-strings/buddy-strings.java
@@ -1,46 +1,43 @@
 class Solution {
     public boolean buddyStrings(String s, String goal) {
 
-        if (s.length() != goal.length()) {
+        if (s.length() != goal.length())
             return false;
-        }
 
         int first = -1;
         int second = -1;
-        int count = 0;
 
         for (int i = 0; i < s.length(); i++) {
 
             if (s.charAt(i) != goal.charAt(i)) {
 
-                count++;
-
-                if (first == -1) {
+                if (first == -1)
                     first = i;
-                } else {
+                else if (second == -1)
                     second = i;
-                }
+                else
+                    return false;
             }
         }
 
-        if (count == 2) {
-            return s.charAt(first) == goal.charAt(second)
-                && s.charAt(second) == goal.charAt(first);
-        }
-
-        if (count == 0) {
+        if (first == -1) {
 
             int[] freq = new int[26];
 
-            for (int i = 0; i < s.length(); i++) {
-                freq[s.charAt(i) - 'a']++;
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
 
-                if (freq[s.charAt(i) - 'a'] >= 2) {
+                if (freq[c - 'a'] >= 2)
                     return true;
-                }
             }
+
+            return false;
         }
 
-        return false;
+        if (second == -1)
+            return false;
+
+        return s.charAt(first) == goal.charAt(second)
+            && s.charAt(second) == goal.charAt(first);
     }
 }
