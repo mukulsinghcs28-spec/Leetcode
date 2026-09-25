2b540fa25942eca798953efa3795b6e8a991d06b
mukulsinghcs28-spec <mukulsingh62530@gmail.com>
2026-09-25 22:08:22 +0530

    Time: 4 ms (77.10%) | Memory: 43.4 MB (58.84%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,26 +1,18 @@
+import java.util.Arrays;
+
 class Solution {
     public int heightChecker(int[] heights) {
 
-        int[] freq = new int[101];
+        int[] expected = heights.clone();
 
-        for (int h : heights) {
-            freq[h]++;
-        }
+        Arrays.sort(expected);
 
         int count = 0;
-        int expected = 0;
 
-        for (int i = 0; i < heights.length; i++) {
-
-            while (freq[expected] == 0) {
-                expected++;
-            }
-
-            if (heights[i] != expected) {
+        for(int i = 0; i < heights.length; i++) {
+            if(heights[i] != expected[i]) {
                 count++;
             }
-
-            freq[expected]--;
         }
 
         return count;
